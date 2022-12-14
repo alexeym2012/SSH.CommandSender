b3d770091ef558db293f2292124e7013b7fdfd3b
Alexey Murashkevich <alexey@ichannel.io>
2022-12-14 17:46:19 +0200

    dev

---
+++ SSH.CommandSender/Dialogs/AddNewCommandDialog.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace SSH.CommandSender.Dialogs
+{
+    partial class AddNewCommandDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AddNewCommandDialog));
+            this.tableLayoutAddNewCommandMain = new System.Windows.Forms.TableLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // tableLayoutAddNewCommandMain
+            // 
+            this.tableLayoutAddNewCommandMain.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutAddNewCommandMain.ColumnCount = 2;
+            this.tableLayoutAddNewCommandMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 22.22222F));
+            this.tableLayoutAddNewCommandMain.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 77.77778F));
+            this.tableLayoutAddNewCommandMain.Location = new System.Drawing.Point(12, 126);
+            this.tableLayoutAddNewCommandMain.Name = "tableLayoutAddNewCommandMain";
+            this.tableLayoutAddNewCommandMain.RowCount = 4;
+            this.tableLayoutAddNewCommandMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutAddNewCommandMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 71.42857F));
+            this.tableLayoutAddNewCommandMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 0F));
+            this.tableLayoutAddNewCommandMain.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 14.28571F));
+            this.tableLayoutAddNewCommandMain.Size = new System.Drawing.Size(622, 125);
+            this.tableLayoutAddNewCommandMain.TabIndex = 0;
+            // 
+            // AddNewCommandDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(630, 438);
+            this.Controls.Add(this.tableLayoutAddNewCommandMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "AddNewCommandDialog";
+            this.Text = "Add New Command";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutAddNewCommandMain;
+    }
+}
---
+++ SSH.CommandSender/Dialogs/AddNewCommandDialog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SSH.CommandSender.Dialogs
+{
+    public partial class AddNewCommandDialog : Form
+    {
+        public AddNewCommandDialog()
+        {
+            InitializeComponent();
+        }
+    }
+}
